fbb7902b955ed45a326d846625912d6163c10383
kawax <kawaxbiz@gmail.com>
2025-06-17 12:55:02 +0900

    Update ReportQuery.php

--- app/Search/ReportQuery.php
+++ app/Search/ReportQuery.php
@@ -8,8 +8,11 @@ class ReportQuery extends AbstractQuery
 {
     public function init(): void
     {
-        $this->setStartDate(now()->subDays(7)->toDateString());
-        $this->setEndDate(now()->toDateString());
+        // Data is updated with a 3-day delay.
+        // Fetch 7 days of data: from 10 to 3 days ago.
+        // Ensures only complete data is included.
+        $this->setStartDate(now()->subDays(10)->toDateString());
+        $this->setEndDate(now()->subDays(3)->toDateString());
         $this->setDimensions(['date']);
         $this->setRowLimit(1000);
         $this->setStartRow(0);
